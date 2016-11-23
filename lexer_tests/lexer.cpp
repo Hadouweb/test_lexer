f6b930145ff06119528e0c466d993feec6efe2f2
Hadouweb <kengo14@hotmail.fr>
2016-11-23 09:36:38 +0100

    init

---
+++ lexer_tests/lexer.cpp
@@ -0,0 +1,23 @@
+//
+// Created by AK on 13/10/15.
+//
+
+#include "gtest/gtest.h"
+
+#if __cplusplus
+extern "C" {
+#endif
+
+#include "lexer.h"
+
+#if __cplusplus
+}
+#endif
+
+TEST(basic_check, test_eq) {
+    EXPECT_EQ(ft_strlen("ok"), 1);
+}
+
+TEST(basic_check, test_neq) {
+    EXPECT_NE(1, 0);
+}
